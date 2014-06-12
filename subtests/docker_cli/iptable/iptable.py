50781c4eb51f3d676d488f33a07360eb625e5241
timesu <lsu@redhat.com>
2014-06-12 18:07:25 +0800

    Fix few typo issues and adjust code format
    *Add a short sleep in case run too fast to add iptable rule
    
    Signed-off-by: Time Su <lsu@redhat.com>

--- subtests/docker_cli/iptable/iptable.py
+++ subtests/docker_cli/iptable/iptable.py
@@ -1,11 +1,10 @@
 """
 Test output of the host iptable rules of container
-1. Run container and filter its iptables rules on host
-2. Check if the rules is affected or expected
+1. Run container and parse/gather it's iptable rules on host.
+2. Check if rules are affected as expected/are handled properly.
 """
 # Okay to be less-strict for these cautions/warnings in subtests
 # pylint: disable=C0103,C0111,R0904,C0103
-
 
 from dockertest.dockercmd import NoFailDockerCmd
 from dockertest.dockercmd import DockerCmd
@@ -15,11 +14,12 @@ from dockertest.subtest import SubSubtest
 from dockertest.subtest import SubSubtestCallerSimultaneous
 from autotest.client import utils
 import re
-
+import time
 
 class iptable(SubSubtestCallerSimultaneous):
 
     pass
+
 
 class iptable_base(SubSubtest):
 
@@ -57,11 +57,9 @@ class iptable_base(SubSubtest):
         """
         Temp method to get container net device name
         """
-        vnet = []
         tmp_cmd = 'brctl show'
         cmd_result = utils.run(tmp_cmd)
-        p = re.compile(r'veth\w+')
-        vnet = p.findall(cmd_result.stdout)
+        vnet = re.findall(r'veth\w+',cmd_result.stdout)
 
         return vnet
 
@@ -77,10 +75,11 @@ class iptable_base(SubSubtest):
         net_device = vnet
         container_rule = []
         tmp_rules = utils.run(tmp_cmd)
-        tmp_rules_list = tmp_rules.stdout.split('\n')
+        tmp_rules_list = tmp_rules.stdout.splitlines()
+
         for rule in tmp_rules_list:
             if net_device in rule:
-                line = rule.split('\n')[0]
+                line = rule
                 container_rule.append(line)
 
         return container_rule
@@ -94,9 +93,9 @@ class iptable_base(SubSubtest):
         super(iptable_base, self).run_once()
 
         subargs = self.sub_stuff['subargs']
-        container = NoFailDockerCmd(self.parent_subtest,
-                                           'run -d -t',
-                                           subargs, verbose=True).execute()
+        NoFailDockerCmd(self.parent_subtest,
+                        'run -d -t',
+                        subargs, verbose=True).execute()
 
     def postprocess(self):
         super(iptable_base, self).postprocess()
@@ -109,26 +108,28 @@ class iptable_base(SubSubtest):
             dcmd = DockerCmd(self.parent_subtest,
                                  'rm',
                                  ['--force',
+                                  '--volume',
                                  self.sub_stuff['name']])
             dcmd.execute()
 
+
 class iptable_remove(iptable_base):
     """
-    Test if container iptable rules removed after stopped
+    Test if container iptable rules are removed after stopped
     """
     def run_once(self):
         super(iptable_remove, self).run_once()
 
-        added_rules = []
-        removed_rules = []
-        net_device_list = self.sub_stuff['net_device_list']
+        time.sleep(3)
+        net_device_list = set(self.read_veth_device()).\
+                             difference(self.sub_stuff['net_device_list'])
+        self.failif(len(net_device_list) != 1,
+                    "Can't obtain network device of the tested container,"
+                    "difference of list of net devices before/after is %s"
+                    % net_device_list)
+        net_device = net_device_list.pop()
 
-        if iptable_base.read_veth_device() == net_device_list:
-            net_device = self.sub_stuff['net_device_list']
-        else:
-            net_device = list(set(self.read_veth_device()) - set(net_device_list)).pop()
-
-        added_rules = iptable_base.read_iptable_rules(net_device)
+        added_rules = self.read_iptable_rules(net_device)
         self.loginfo("Container %s\niptable rule list %s:" %
                       (self.sub_stuff['name'], added_rules))
 
@@ -136,20 +137,17 @@ class iptable_remove(iptable_base):
                         'stop',
                         [self.sub_stuff['name']]).execute()
 
-        removed_rules = iptable_base.read_iptable_rules(net_device)
+        removed_rules = self.read_iptable_rules(net_device)
         if added_rules == []:
             self.sub_stuff['result'] = False
-            self.sub_stuff['result_info'] = """
-                                            No rules added when container start
-                                            """
+            self.sub_stuff['result_info'] = ("No rules added when container "
+                                             "started.")
+
         elif removed_rules:
             self.sub_stuff['result'] = False
-            self.sub_stuff['result_info'] = """
-                                            Container %s
-                                            iptables not removed after stopped
-                                            Iptable %s
-                                            """ \
-                                            % (self.sub_stuff['name'],
-                                             removed_rules)
-
+            self.sub_stuff['result_info'] = ("Container %s iptables not "
+                                             "removed after stopped "
+                                             "Iptable %s."
+                                             % (self.sub_stuff['name'],
+                                                removed_rules))
 
